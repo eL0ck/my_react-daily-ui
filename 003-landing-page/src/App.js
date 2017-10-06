12af05367af3cb7e96dd2dcaee8b2bca1edc7968
eL0ck <tpj800@gmail.com>
2017-10-06 15:57:25 +1000

    2: Got basic menupage with singulare logo component inside.

--- 003-landing-page/src/App.js
+++ 003-landing-page/src/App.js
@@ -1,22 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import MenuPage from "./components/MenuPage"
 
-class App extends Component { // eslint-disable-line react/prefer-stateless-function
-  /* Keep as Class for now until use of state can be determined */
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+const App = MenuPage
+export default App
---
+++ 003-landing-page/src/components/Logo/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Logo = () =>
+ <p>Hellow foool</p>
+
+export default Logo
