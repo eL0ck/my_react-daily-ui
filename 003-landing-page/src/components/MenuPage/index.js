6000c1e8da30cc9c64b47ba21f9e8b82257d6a97
eL0ck <tpj800@gmail.com>
2017-10-08 20:10:52 +1000

    9b: TitleList prop being filled from MainPage state variable. Now just to do the query and display the Component

--- 003-landing-page/src/components/MenuPage/index.js
+++ 003-landing-page/src/components/MenuPage/index.js
@@ -9,7 +9,20 @@ import Hero from '../Hero'
 class MenuPage extends Component {
   constructor(props) {
     super(props)
-    this.state = { searchQuery: "" }
+    this.state = { searchQuery: "", temp: ""}
+  }
+
+  handleSearchInput = e => {
+    console.log("input change handler fired ... ")
+    this.setState({ temp: e.target.value })
+  }
+
+  fireSearchOnEnter = e => {
+    if (e.key === 'Enter' ){
+      console.log("Enter key pressed. Searching ... ")
+      console.log("Search Term is", this.state.temp );
+      this.setState({ searchQuery: this.state.temp })
+    }
   }
 
   render() {
@@ -19,7 +32,11 @@ class MenuPage extends Component {
           <Logo />
           <Navigation />
           <div id="search" className="Search">
-            <input type="search" placeholder="Search for a title..." />
+            <input type="search"
+              onChange={ this.handleSearchInput }
+              onKeyUp={ this.fireSearchOnEnter}
+              placeholder="Search for a title..."
+              value={ this.state.temp } />
           </div>
           <UserProfile />
         </header>
