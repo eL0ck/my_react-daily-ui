d136df5d9d237340f38b32287d72d1bbf1271998
eL0ck <tpj800@gmail.com>
2017-10-08 19:07:46 +1000

    9a: The first step to allowing a TitleList for search is to allow the passing of a query string into the component.  The query string will come from another component in the `MenuPage` via its `state`.  To allow state we have to initialise it and this requires converting the component to a class.

--- 003-landing-page/src/components/MenuPage/index.js
+++ 003-landing-page/src/components/MenuPage/index.js
@@ -1,28 +1,38 @@
 import './styles.css'
-import React from 'react'
+import React, { Component } from 'react'
 import Logo from '../Logo'
 import TitleList from '../TitleList'
 import UserProfile from '../UserProfile'
 import Navigation from '../Navigation'
 import Hero from '../Hero'
 
-const MenuPage = () => (
-  <div>
-    <header className="Header">
-      <Logo />
-      <Navigation />
-      <div id="search" className="Search">
-        <input type="search" placeholder="Search for a title..." />
+class MenuPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { searchQuery: "" }
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="Header">
+          <Logo />
+          <Navigation />
+          <div id="search" className="Search">
+            <input type="search" placeholder="Search for a title..." />
+          </div>
+          <UserProfile />
+        </header>
+        <Hero />
+        <TitleList name="Search Results" query={this.state.searchQuery}/>
+        <TitleList name="The Simpsons" />
+        <TitleList name="Comedy" />
+        <TitleList name="Sci-Fi greats" />
+        <TitleList name="Horror" />
+        <TitleList name="Top TV picks for Jack" />
       </div>
-      <UserProfile />
-    </header>
-    <Hero />
-    <TitleList name="The Simpsons" />
-    <TitleList name="Comedy" />
-    <TitleList name="Sci-Fi greats" />
-    <TitleList name="Horror" />
-    <TitleList name="Top TV picks for Jack" />
-  </div>
-)
+    )
+  }
+}
 
 export default MenuPage
